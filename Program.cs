736942b9b4f0e075577fa9a54d3e48bc4ef1e2e4
Maria Eduarda Casalunga <mariaeduardacasalunga@gmail.com>
2022-05-01 01:28:19 -0300

    Finaliza exercício

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿int NumeroDigitado;
+Console.Write("digite um número: ");
+NumeroDigitado = Convert.ToInt32(Console.ReadLine()!);
+
+if (NumeroDigitado >=0)
+{}
+else 
+{
+    Console.WriteLine("Você digitou um número negativo.");
+}
+
